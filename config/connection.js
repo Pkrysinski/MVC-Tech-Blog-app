8eaefb29657b902eb77673e09133ab81aef1d1c8
Peter Krysinski <pakrysin@gmail.com>
2023-03-08 15:53:44 -0700

    Updating connection.js variables

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
